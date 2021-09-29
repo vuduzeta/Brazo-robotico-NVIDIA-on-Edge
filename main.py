3984160c9506ff08fa96769d5eaeb572fa0161f5
Juan.Gamboa <juan.gamboa.g@usach.cl>
2021-09-30 06:50:47 +0800

    Se crean los métodos para mover el brazo a la derecha centro e izquierda.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def arm_retracted(s_time = 500):
     Arm.Arm_serial_servo_write(4, 0, s_time)
     time.sleep(.01)
     Arm.Arm_serial_servo_write(5, 90, int(s_time*1.2))
-    time.sleep(.1)
+    time.sleep(s_time/1000)
 
 def arm_center(s_time = 500):
     Arm.Arm_serial_servo_write(2, 66, s_time)
@@ -22,7 +22,7 @@ def arm_center(s_time = 500):
     Arm.Arm_serial_servo_write(4, 29, s_time)
     time.sleep(.01)
     Arm.Arm_serial_servo_write(5, 270, int(s_time*1.2))
-    time.sleep(.1)
+    time.sleep(s_time/1000)
 
 def arm_extended(s_time = 500):
     Arm.Arm_serial_servo_write(2, 22, s_time)
@@ -32,14 +32,34 @@ def arm_extended(s_time = 500):
     Arm.Arm_serial_servo_write(4, 56, s_time)
     time.sleep(.01)
     Arm.Arm_serial_servo_write(5, 270, int(s_time*1.2))
-    time.sleep(.1)
+    time.sleep(s_time/1000)
 
+def arm_move_right(s_time = 500):
+    Arm.Arm_serial_servo_write(1, 0, s_time)
+    time.sleep(s_time/1000)
+
+def arm_move_center(s_time = 500):
+    Arm.Arm_serial_servo_write(1, 90, s_time)
+    time.sleep(s_time/1000)
+
+def arm_move_left(s_time = 500):
+    Arm.Arm_serial_servo_write(1, 180, s_time)
+    time.sleep(s_time/1000)
 
 arm_extended(1000)
+arm_move_right(1000)
+arm_move_center(1000)
+arm_move_left(1000)
 time.sleep(2)
 arm_center(1000)
+arm_move_right(1000)
+arm_move_center(1000)
+arm_move_left(1000)
 time.sleep(2)
 arm_retracted(1000)
+arm_move_right(1000)
+arm_move_center(1000)
+arm_move_left(1000)
 
 
 
